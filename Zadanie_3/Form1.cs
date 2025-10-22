76f02eaf2591a66c12977d04b9d3734f1d6d06eb
Kacper Stasiak <stasiak24166@stud.akademiabialska.pl>
2025-10-22 10:20:09 +0200

    Zmiany w gui

--- Zadanie_3/Form1.cs
+++ Zadanie_3/Form1.cs
@@ -10,11 +10,53 @@ using System.Windows.Forms;
 
 namespace Zadanie_3
 {
-    public partial class Form1 : Form
+    public partial class Form : System.Windows.Forms.Form
     {
-        public Form1()
+        public Form()
         {
             InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void capacityBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void actBut_Click(object sender, EventArgs e)
+        {
+            string capacityText = capacityBox.Text;
+            string concetText = concentrationBox.Text;
+
+            double capacity = double.Parse(capacityText);
+            double concen = double.Parse(concetText);
+
+
+            //Walidacja programu
+            if (capacity < 0 || concen < 0 || concen > 100)
+            {
+                solutionBox.Text = "Poza zakresem";
+                MessageBox.Show("Pojemność nie może być ujemne, a stężnie musi być w zakrsie 0-100%!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            //Zmienna przechowująca stężenie jako procent 
+            double concenU = concen / 100;
+
+            //Obliczenie
+            double wynik = capacity * concenU;
+
+            //Wyświetlenie wyniku
+            solutionBox.Text = wynik.ToString("F2") + "L";
+        }
+
+        private void solutionBox_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
